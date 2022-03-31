2b8ad61ba90a4b1f0c2481a0e8971d7ae6008217
Andre Pham <andrepham33@gmail.com>
2022-03-31 13:15:58 +1100

    Fixed TextToSpeechEngine initialisation bug that caused the first method call to fail, took speak calls out of bindings because bindings are called in the background on initialisation

--- app/src/main/java/com/github/talkbacktutorial/MainActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/MainActivity.kt
@@ -6,13 +6,11 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.Observable
-import androidx.lifecycle.Observer
 import com.github.talkbacktutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val ttsEngine = TextToSpeechEngine(this)
+    private lateinit var ttsEngine: TextToSpeechEngine
     private lateinit var lesson1Card: CardView
     private lateinit var lesson2Card: CardView
     private lateinit var lesson3Card: CardView
@@ -28,39 +26,35 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lessonsModel = this.lessonsModel
 
+        this.ttsEngine = TextToSpeechEngine(this)
         this.lesson1Card = binding.lesson1Card.lessonCard
         this.lesson2Card = binding.lesson2Card.lessonCard
         this.lesson3Card = binding.lesson3Card.lessonCard
         this.lesson4Card = binding.lesson4Card.lessonCard
 
         lessonsModel.lesson1Locked.observe(this) { value ->
-            if (binding.lesson1Card.locked == value) return@observe
             binding.lesson1Card.locked = value
-            this.ttsEngine.speak("Lesson 1 ${if (value) "locked" else "unlocked"}")
         }
         lessonsModel.lesson2Locked.observe(this) { value ->
-            if (binding.lesson2Card.locked == value) return@observe
             binding.lesson2Card.locked = value
-            this.ttsEngine.speak("Lesson 2 ${if (value) "locked" else "unlocked"}")
         }
         lessonsModel.lesson3Locked.observe(this) { value ->
-            if (binding.lesson3Card.locked == value) return@observe
             binding.lesson3Card.locked = value
-            this.ttsEngine.speak("Lesson 3 ${if (value) "locked" else "unlocked"}")
         }
         lessonsModel.lesson4Locked.observe(this) { value ->
-            if (binding.lesson4Card.locked == value) return@observe
             binding.lesson4Card.locked = value
-            this.ttsEngine.speak("Lesson 4 ${if (value) "locked" else "unlocked"}")
         }
 
         this.lesson1Card.setOnClickListener {
+            if (this.lessonsModel.lesson2Locked.value!!) this.ttsEngine.speak("Lesson 2 unlocked")
             this.lessonsModel.lesson2Locked.value = false
         }
         this.lesson2Card.setOnClickListener {
+            if (this.lessonsModel.lesson3Locked.value!!) this.ttsEngine.speak("Lesson 3 unlocked")
             this.lessonsModel.lesson3Locked.value = false
         }
         this.lesson3Card.setOnClickListener {
+            if (this.lessonsModel.lesson4Locked.value!!) this.ttsEngine.speak("Lesson 4 unlocked")
             this.lessonsModel.lesson4Locked.value = false
         }
         this.lesson4Card.setOnClickListener {
--- app/src/main/java/com/github/talkbacktutorial/TextToSpeechEngine.kt
+++ app/src/main/java/com/github/talkbacktutorial/TextToSpeechEngine.kt
@@ -12,35 +12,29 @@ class TextToSpeechEngine(context: AppCompatActivity) {
         private var history = ArrayList<String>()
     }
 
-    private val ttsEngine: TextToSpeech by lazy {
-        TextToSpeech(context) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                ttsEngine.language = Locale.getDefault()
-                try {
-                    val systemPitch = Settings.Secure.getInt(context.contentResolver, Settings.Secure.TTS_DEFAULT_PITCH)
-                    val systemRate = Settings.Secure.getInt(context.contentResolver, Settings.Secure.TTS_DEFAULT_RATE)
-                    ttsEngine.setPitch(systemPitch/100F)
-                    ttsEngine.setSpeechRate(systemRate/100F)
-                } catch (e: Settings.SettingNotFoundException) { /* Default values are set instead */ }
-            }
-        }
-    }
+    private val ttsEngine: TextToSpeech
 
     val isSpeaking: Boolean
         get() = this.ttsEngine.isSpeaking
 
-    fun speak(text: String) {
-        this.ttsEngine.speak(text, TextToSpeech.QUEUE_ADD, null, "tts1")
-        TextToSpeechEngine.history.add(text)
+    init {
+        this.ttsEngine = TextToSpeech(context) { }
+        this.ttsEngine.language = Locale.getDefault()
+        try {
+            val systemPitch = Settings.Secure.getInt(context.contentResolver, Settings.Secure.TTS_DEFAULT_PITCH)
+            val systemRate = Settings.Secure.getInt(context.contentResolver, Settings.Secure.TTS_DEFAULT_RATE)
+            ttsEngine.setPitch(systemPitch/100F)
+            ttsEngine.setSpeechRate(systemRate/100F)
+        } catch (e: Settings.SettingNotFoundException) { /* Default values are set instead */ }
     }
 
-    fun speakWithOverride(text: String) {
-        this.ttsEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts1")
+    fun speak(text: String, override: Boolean = false) {
+        this.ttsEngine.speak(text, if (override) TextToSpeech.QUEUE_FLUSH else TextToSpeech.QUEUE_ADD, null, "tts1")
         TextToSpeechEngine.history.add(text)
     }
 
     fun repeatLast(amount: Int = 1) {
-        for (index in amount downTo 1) {
+        for (index in 1..amount) {
             if (TextToSpeechEngine.history.size < index) continue
             this.ttsEngine.speak(TextToSpeechEngine.history[index-1], TextToSpeech.QUEUE_ADD, null, "tts1")
         }
