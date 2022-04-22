c5eecf576dab770660fdf82c22125a791711d5f4
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-04-22 12:55:07 +1000

    Added template for needed modules/lesson

---
+++ app/src/main/java/com/github/talkbacktutorial/lessons/Lesson5.kt
@@ -0,0 +1,11 @@
+package com.github.talkbacktutorial.lessons
+
+import com.github.talkbacktutorial.lessons.modules.Module
+
+class Lesson5 : Lesson() {
+    override val title: String = "Lesson 5: Advanced Menu Navigation"
+    override val sequenceNumeral: Int = 5
+    override val modules: ArrayList<Module> = ArrayList(listOf(
+        // Add more ...
+    ))
+}
---
+++ app/src/main/java/com/github/talkbacktutorial/lessons/modules/OpenRecentAppsModule.kt
@@ -0,0 +1,18 @@
+package com.github.talkbacktutorial.lessons.modules
+
+import android.content.Context
+import android.content.Intent
+import com.github.talkbacktutorial.activities.modules.TodoModuleXModuleActivity
+
+/**
+ * TODO
+ */
+class OpenRecentAppsModule : Module() { // E.g. PausingAndPlayingMediaModule
+
+    override val title: String = "TODO MODULE X" // E.g. "Pause and Play Media"
+
+    override fun startActivity(context: Context) {
+        context.startActivity(Intent(context, TodoModuleXModuleActivity::class.java))
+    }
+
+}
