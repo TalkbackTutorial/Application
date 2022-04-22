091bff421ffa2ea9bdb891e8504e68280bd157cc
jaiclapp <jcla0016@student.monash.edu>
2022-04-22 12:55:21 +1000

    Added Lesson 5 template box - added to Lesson container

---
+++ app/src/main/java/com/github/talkbacktutorial/lessons/Lesson5.kt
@@ -0,0 +1,14 @@
+package com.github.talkbacktutorial.lessons
+
+import com.github.talkbacktutorial.lessons.modules.Module
+import java.util.ArrayList
+
+class Lesson5 : Lesson() {
+
+    override val title: String = "Advanced Menu Navigation"
+    override val sequenceNumeral: Int = 5
+    override val modules: ArrayList<Module> = ArrayList(listOf(
+        // Add more ...
+    ))
+
+}
