84c1b1a1151596f7412739c12c6a3caca61e2c9a
Chokss54 <joelyang54@gmail.com>
2022-04-28 08:10:42 +0800

    - Created Activity for lesson 3 - Created different fragments to be implemented as UI for users to interact

---
+++ app/src/main/java/com/github/talkbacktutorial/activities/Lesson3/Lesson3Activity.kt
@@ -0,0 +1,12 @@
+package com.github.talkbacktutorial.activities.Lesson3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.talkbacktutorial.R
+
+class Lesson3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson3)
+    }
+}
