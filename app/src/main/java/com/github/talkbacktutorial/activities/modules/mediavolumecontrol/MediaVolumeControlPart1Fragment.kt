c1161ec7b83fdb5d9d640c8cef0c77603054699d
nlaw0004 <nlaw0004@student.monash.edu>
2022-04-28 12:28:37 +1000

    basic media control layout and res

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/mediavolumecontrol/MediaVolumeControlPart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/mediavolumecontrol/MediaVolumeControlPart1Fragment.kt
@@ -1,6 +1,14 @@
 package com.github.talkbacktutorial.activities.modules.mediavolumecontrol
 
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
 import com.github.talkbacktutorial.databinding.FragmentMediaVolumeControlModulePart1Binding
 
@@ -13,4 +21,70 @@ class MediaVolumeControlPart1Fragment: Fragment() {
 
     private lateinit var binding: FragmentMediaVolumeControlModulePart1Binding
     private lateinit var ttsEngine: TextToSpeechEngine
+
+    private var mediaPlayer:MediaPlayer? = null
+    private var totalSongDuration:Int = 0
+    private var seekbarVolume:SeekBar? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_media_volume_control_module_part1, container, false)
+        return this.binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.ttsEngine = TextToSpeechEngine((activity as MediaVolumeControlModuleActivity)).onFinishedSpeaking(triggerOnce = true){
+        }
+
+        this.seekbarVolume = this.binding.volumeControlSeekBar
+        mediaControls()
+    }
+
+    private fun mediaControls(){
+        this.binding.fabPlay.setOnClickListener{
+            if(mediaPlayer == null){
+                mediaPlayer = MediaPlayer.create(context, R.raw.media_sound)
+            }
+
+            mediaPlayer?.start()
+            mediaPlayer?.isLooping = true
+            mediaPlayer?.setVolume(0.5f, 0.5f)
+            totalSongDuration = mediaPlayer!!.duration
+
+        }
+
+        this.binding.fabStop.setOnClickListener{
+            if(mediaPlayer !== null) {
+                mediaPlayer?.stop()
+                mediaPlayer?.reset()
+                mediaPlayer?.release()
+                mediaPlayer = null
+            }
+        }
+
+        this.seekbarVolume?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if(p2) {
+                    var volumeNum = p1/100.0f
+                    mediaPlayer?.setVolume(volumeNum, volumeNum)
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        this.ttsEngine.shutDown()
+        super.onDestroyView()
+    }
 }
