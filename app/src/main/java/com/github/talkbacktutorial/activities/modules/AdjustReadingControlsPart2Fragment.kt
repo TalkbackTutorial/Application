ceb1c0c1d9019485b0787e52a56562f7465ded09
Chokss54 <joelyang54@gmail.com>
2022-04-28 10:24:39 +0800

    - Added more Fragments for different modes that user will use to learn the interactions (e.g. Headers, Controls, characters etc..)
    - NOTE: contents of each fragment layouts and contents for text2speech integration will be further implemented

---
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/AdjustReadingControlsPart2Fragment.kt
@@ -0,0 +1,53 @@
+package com.github.talkbacktutorial.activities.modules
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.github.talkbacktutorial.R
+import com.github.talkbacktutorial.TextToSpeechEngine
+import com.github.talkbacktutorial.databinding.FragmentAdjustReadingControlsPart1Binding
+import com.github.talkbacktutorial.databinding.FragmentAdjustReadingControlsPart2Binding
+
+/**
+ * Instantiates a UI for user to interact with header mode
+ *
+ * @author Joel Yang
+ * @see AdjustReadingControlsActivity
+ */
+class AdjustReadingControlsPart2Fragment : Fragment() {
+    private lateinit var binding: FragmentAdjustReadingControlsPart2Binding
+    private lateinit var ttsEngine: TextToSpeechEngine
+
+    // callback run after this fragment is created for AdjustReadingControlsActivity
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+        /*
+            Here, we inflate our layout file (basically, turning the XML into a UI) through
+            DataBindingUtil, which will provide our layout binding during the inflate process.
+         */
+        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_adjust_reading_controls_part2, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.speakIntro()
+
+    }
+
+    private fun speakIntro() {
+        val intro = """
+        """.trimIndent()
+        this.ttsEngine.speakOnInitialisation(intro)
+    }
+
+    override fun onDestroyView() {
+        this.ttsEngine.shutDown()
+        super.onDestroyView()
+    }
+}
