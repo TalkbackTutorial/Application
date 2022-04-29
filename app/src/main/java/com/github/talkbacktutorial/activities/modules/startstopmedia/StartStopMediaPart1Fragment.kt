0139a049cd309ebc842ebd7c23cd8fe2e3935305
Sandy Du <sduu0002@student.monash.edu>
2022-04-29 18:49:01 +1000

    Added video view to start/stop media module fragment

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/startstopmedia/StartStopMediaPart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/startstopmedia/StartStopMediaPart1Fragment.kt
@@ -1,6 +1,14 @@
 package com.github.talkbacktutorial.activities.modules.startstopmedia
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
 import com.github.talkbacktutorial.databinding.FragmentStartStopMediaModulePart1Binding
 
@@ -13,4 +21,34 @@ class StartStopMediaPart1Fragment: Fragment() {
 
     private lateinit var binding: FragmentStartStopMediaModulePart1Binding
     private lateinit var ttsEngine: TextToSpeechEngine
+    private lateinit var videoView: VideoView
+    private var mediaController: MediaController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_start_stop_media_module_part1, container, false)
+        return this.binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.ttsEngine = TextToSpeechEngine((activity as StartStopMediaModuleActivity)).onFinishedSpeaking(triggerOnce = true){
+        }
+
+        // Get videoView by id
+        this.videoView = this.binding.videoView
+        videoView.setVideoPath("android.resource://" + requireActivity().packageName + "/" + R.raw.video_test)
+        mediaController = MediaController(context)
+        mediaController?.setEnabled(true)
+        mediaController?.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        this.videoView.start()
+    }
+
+
+
 }
