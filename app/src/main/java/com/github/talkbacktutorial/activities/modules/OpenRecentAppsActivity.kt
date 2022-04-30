18c5c39dde3cd7ace3e7e2b1461b2a8b6ed208db
jaiclapp <jcla0016@student.monash.edu>
2022-04-30 17:03:30 +1000

    Updated OpenRecentAppsActivity
    - Back button capabilities added

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/OpenRecentAppsActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/OpenRecentAppsActivity.kt
@@ -1,10 +1,12 @@
 package com.github.talkbacktutorial.activities.modules
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import com.github.talkbacktutorial.R
+import com.github.talkbacktutorial.activities.MainActivity
 import com.github.talkbacktutorial.databinding.ActivityModuleOpenRecentAppsBinding
 
 class OpenRecentAppsActivity : AppCompatActivity() {
@@ -18,6 +20,17 @@ class OpenRecentAppsActivity : AppCompatActivity() {
         this.binding = DataBindingUtil.setContentView(this, R.layout.activity_module_open_recent_apps)
         supportFragmentManager.commit {
             replace(R.id.frame, OpenRecentAppsPart1Fragment())
+            addToBackStack("openRecentAppsModulePart1")
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount != 1) {
+            super.onBackPressed()
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
         }
     }
 
