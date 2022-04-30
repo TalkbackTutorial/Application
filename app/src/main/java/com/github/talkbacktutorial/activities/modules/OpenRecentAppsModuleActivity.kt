de3e0b1d7ce03c994f964a0833279348f9a219d6
jaiclapp <jcla0016@student.monash.edu>
2022-04-30 15:36:40 +1000

    Updated OpenRecentAppsModule
    - Renaming classes

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/OpenRecentAppsModuleActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/OpenRecentAppsModuleActivity.kt
@@ -1,14 +1,10 @@
 package com.github.talkbacktutorial.activities.modules
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import com.github.talkbacktutorial.R
-import com.github.talkbacktutorial.activities.MainActivity
-import com.github.talkbacktutorial.activities.lesson0.Lesson0Part1Fragment
-import com.github.talkbacktutorial.databinding.ActivityLesson0Binding
 import com.github.talkbacktutorial.databinding.ActivityModuleOpenRecentAppsBinding
 
 class OpenRecentAppsModuleActivity : AppCompatActivity() {
