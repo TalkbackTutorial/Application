7a1dd5bdaa25e1870692bdd339e5f9860d76f0e1
nlaw0004 <nlaw0004@student.monash.edu>
2022-04-30 16:58:44 +1000

    integrate tts in fragment

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/mediavolumecontrol/MediaVolumeControlPart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/mediavolumecontrol/MediaVolumeControlPart1Fragment.kt
@@ -1,16 +1,26 @@
 package com.github.talkbacktutorial.activities.modules.mediavolumecontrol
 
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.SeekBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
+import com.github.talkbacktutorial.activities.MainActivity
 import com.github.talkbacktutorial.databinding.FragmentMediaVolumeControlModulePart1Binding
+import kotlinx.coroutines.currentCoroutineContext
+
 
 class MediaVolumeControlPart1Fragment: Fragment() {
 
@@ -23,8 +33,11 @@ class MediaVolumeControlPart1Fragment: Fragment() {
     private lateinit var ttsEngine: TextToSpeechEngine
 
     private var mediaPlayer:MediaPlayer? = null
+    private lateinit var audioManager:AudioManager
     private var totalSongDuration:Int = 0
     private var seekbarVolume:SeekBar? = null
+    private var currentVolume: Int = 50
+    private var swipeUp:Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,12 +54,19 @@ class MediaVolumeControlPart1Fragment: Fragment() {
         this.ttsEngine = TextToSpeechEngine((activity as MediaVolumeControlModuleActivity)).onFinishedSpeaking(triggerOnce = true){
         }
 
+        // media setup
+        audioManager = requireActivity().getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
         this.seekbarVolume = this.binding.volumeControlSeekBar
         mediaControls()
+
+        // tts introduction
+        this.speakIntro()
     }
 
     private fun mediaControls(){
         this.binding.fabPlay.setOnClickListener{
+            // when users click on the fab button
             if(mediaPlayer == null){
                 mediaPlayer = MediaPlayer.create(context, R.raw.media_sound)
             }
@@ -55,21 +75,22 @@ class MediaVolumeControlPart1Fragment: Fragment() {
             mediaPlayer?.isLooping = true
             mediaPlayer?.setVolume(0.5f, 0.5f)
             totalSongDuration = mediaPlayer!!.duration
+            seekbarVolume?.progress = 50
 
         }
 
         this.binding.fabStop.setOnClickListener{
-            if(mediaPlayer !== null) {
-                mediaPlayer?.stop()
-                mediaPlayer?.reset()
-                mediaPlayer?.release()
-                mediaPlayer = null
-            }
+            stopMedia()
         }
 
+        this.seekbarVolume?.setOnClickListener{
+            val info = "Media Volume Slider. Swipe up or down to increase or decrease the volume".trimIndent()
+            this.ttsEngine.speak(info)
+        }
         this.seekbarVolume?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 if(p2) {
+                    currentVolume = p1
                     var volumeNum = p1/100.0f
                     mediaPlayer?.setVolume(volumeNum, volumeNum)
                 }
@@ -79,8 +100,67 @@ class MediaVolumeControlPart1Fragment: Fragment() {
             }
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                if(currentVolume >= 75){
+                    val info = """
+                        Now that you learned how to increase the volume, we'll try decreasing the volume to 25 or below.
+                        To decrease the volume, swipe down.
+                    """.trimIndent()
+
+                    ttsEngine.speak(info)
+                    swipeUp = true
+                }
+
+                if(currentVolume <= 25 && swipeUp){
+                    //TODO activate finish lesson button
+                        // insert finish/continue button
+                    ttsEngine.speak("Volume below 25")
+                    insertFinishButton()
+                }
+
             }
         })
+    }
+
+    private fun stopMedia(){
+        if(mediaPlayer !== null) {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
+    }
+
+     private fun insertFinishButton(){
+         val constraintLayout = this.binding.mediaVolumeControlConstraintLayout
+         val finishButton = Button(requireContext())
+         finishButton.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+         finishButton.text = "Finish"
+         finishButton.setOnClickListener(View.OnClickListener {
+             endLesson()
+         })
+         constraintLayout.addView(finishButton);
+     }
+
+    private fun speakIntro(){
+        //TODO retype the introduction
+        val intro = """
+            In this tutorial, you will be learning how to control the media volume sliders, increasing or decreasing your phone's media volume. 
+            To start, explore your screen by touch and you find the play button. 
+            Play the music.
+            Then, increase the volume to 75 or above by swiping up.
+            """.trimIndent()
+
+        this.ttsEngine.speakOnInitialisation(intro)
+    }
+
+    private fun endLesson(){
+        /* Lesson's complete go back to Main Activity */
+        val intent = Intent((activity as MediaVolumeControlModuleActivity), MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        stopMedia()
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
