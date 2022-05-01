2a3508afd83415d4624be0500223b5cab6fcb4b7
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-05-01 18:04:11 +1000

    fixed import error

--- app/src/main/java/com/github/talkbacktutorial/lessons/modules/OpenVoiceCommandModule.kt
+++ app/src/main/java/com/github/talkbacktutorial/lessons/modules/OpenVoiceCommandModule.kt
@@ -2,6 +2,7 @@ package com.github.talkbacktutorial.lessons.modules
 
 import android.content.Context
 import android.content.Intent
+import com.github.talkbacktutorial.activities.modules.OpenVoiceCommandModuleActivity
 import com.github.talkbacktutorial.activities.modules.TodoModuleXModuleActivity
 
 /**
