b392ee3b0df8cfa9bb49046395d34fee059fdbc6
Jade Davis <jdav0023@student.monash.edu>
2022-05-02 23:15:36 +1000

    Finishing logic for detecting when max and min has been reached

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/adjustslider/AdjustSliderModulePart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/adjustslider/AdjustSliderModulePart1Fragment.kt
@@ -1,22 +1,15 @@
 package com.github.talkbacktutorial.activities.modules.adjustslider
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.accessibility.AccessibilityEvent
 import android.widget.SeekBar
-import android.widget.TextView
-import androidx.core.view.allViews
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
-import com.github.talkbacktutorial.activities.modules.scrolling.ScrollingModuleActivity
-import com.github.talkbacktutorial.activities.modules.scrolling.ScrollingModulePart1Fragment
 import com.github.talkbacktutorial.databinding.FragmentAdjustSliderModulePart1Binding
-import com.github.talkbacktutorial.databinding.FragmentScrollingModulePart1Binding
 
 class AdjustSliderModulePart1Fragment : Fragment() {
 
@@ -27,16 +20,18 @@ class AdjustSliderModulePart1Fragment : Fragment() {
 
     private lateinit var binding: FragmentAdjustSliderModulePart1Binding
     private lateinit var ttsEngine: TextToSpeechEngine
-    lateinit var menuSlider: SeekBar
-//    lateinit var menuSliderText: TextView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    lateinit var menuSlider: SeekBar
+    var currentSliderValue: Int = 0
+    var maxValue: Int = 100
+    var minValue: Int = 0
+    var hasReachedMax = false
+    var hasReachedMin = false
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_adjust_slider_module_part1, container, false)
         this.menuSlider = this.binding.menuSlider
-//        this.menuSliderText = this.binding.seekBarValueTV
+        currentSliderValue = getSliderValue()
         return binding.root
     }
 
@@ -46,8 +41,51 @@ class AdjustSliderModulePart1Fragment : Fragment() {
             .onFinishedSpeaking(triggerOnce = true) {
                 this.menuSlider.visibility = View.VISIBLE
                 setSliderHandler()
+                speakGoToMax()
             }
         this.speakIntro()
+    }
+
+    override fun onDestroyView() {
+        this.ttsEngine.shutDown()
+        super.onDestroyView()
+    }
+
+    /**
+     * Retrieves the current value of the slider
+     * @author Jade Davis
+     */
+    private fun getSliderValue(): Int {
+        return this.menuSlider.progress
+    }
+
+    /**
+     * Sets the onChange event handler for the seekBar. Updates the class attribute which stores
+     * the current progress of the menu bar and handles the logic for knowing when the max and min
+     * have been reached, and when to allow the next portion of the module to continue.
+     * @author Jade Davis
+     */
+    private fun setSliderHandler() {
+        this.menuSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                binding.seekBarValueTV.setText(i.toString())
+                currentSliderValue = i
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                if (currentSliderValue == maxValue && !hasReachedMax) {
+                    hasReachedMax = true
+                    speakGoToMin()
+                } else if (currentSliderValue == minValue && hasReachedMax) {
+                    hasReachedMin = true
+                    speakOutro()    // TODO: Replace with appropriate action to take to next fragment
+                }
+            }
+        })
     }
 
     /**
@@ -56,29 +94,41 @@ class AdjustSliderModulePart1Fragment : Fragment() {
      */
     private fun speakIntro() {
         val intro = """
-            Testing testing testing.
+            Welcome.
         """.trimIndent()
         this.ttsEngine.speakOnInitialisation(intro)
     }
 
-    override fun onDestroyView() {
-        this.ttsEngine.shutDown()
-        super.onDestroyView()
+    /**
+     *
+     * @author Antony Loose
+     */
+    private fun speakGoToMax() {
+        val goToMax = """
+            Go to max.
+        """.trimIndent()
+        this.ttsEngine.speakOnInitialisation(goToMax)
     }
 
-    fun setSliderHandler() {
-        this.menuSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+    /**
+     *
+     * @author Antony Loose
+     */
+    private fun speakGoToMin() {
+        val goToMin = """
+            Go to min.
+        """.trimIndent()
+        this.ttsEngine.speakOnInitialisation(goToMin)
+    }
 
-            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                binding.seekBarValueTV.setText(i.toString())
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
-            }
-        })
+    /**
+     * Speaks an outro for the fragment.
+     * @author Jade Davis
+     */
+    private fun speakOutro() {
+        val outro = """
+            Well done.
+        """.trimIndent()
+        this.ttsEngine.speakOnInitialisation(outro)
     }
 }
