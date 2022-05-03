f21bda5a50b43fe4d86c8a1e261cd6bdaec7bf0e
Antony <86053464+AntonyLoose@users.noreply.github.com>
2022-05-03 13:44:09 +1000

    Added documentation to AdjustSliderDelegate

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/adjustslider/AdjustSliderDelegate.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/adjustslider/AdjustSliderDelegate.kt
@@ -8,6 +8,11 @@ import android.widget.SeekBar
 import com.github.talkbacktutorial.TextToSpeechEngine
 
 /**
+ * This delegate allows us to execute code before talkback reads screen. This is needed as in some cases talkback will interrupt tts and therefore,
+ * the user will not be read instructions.
+ * This should be used to tell the user to go to min after they reach max and to tell them they've completed the task after reaching min
+ * @param maxVal the value at which you want to trigger the first instruction
+ * @param minVal the value at which you want to trigger the end of the fragment
  * @author Antony Loose
  */
 class AdjustSliderDelegate(maxVal: Int, minVal: Int) : View.AccessibilityDelegate() {
