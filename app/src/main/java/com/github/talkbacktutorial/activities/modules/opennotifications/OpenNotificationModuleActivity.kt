d032d2e419710039b7686974e82c7a776b973d30
Your Name <thuynh2001.dw@gmail.com>
2022-05-05 13:52:11 +1000

    testing imports

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleActivity.kt
@@ -1,4 +1,12 @@
 package com.github.talkbacktutorial.activities.modules.opennotifications
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commit
+import com.github.talkbacktutorial.R
+import com.github.talkbacktutorial.activities.MainActivity
+
 class OpenNotificationModuleActivity {
 }
