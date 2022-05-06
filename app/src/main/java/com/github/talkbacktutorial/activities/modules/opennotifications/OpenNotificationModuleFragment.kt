0647dec38b1af963384ce6df8428e94d886c7cae
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2022-05-07 02:06:15 +1000

    Add a listener to provide different feedback everytime the user open/close notification drawer

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
@@ -1,36 +1,72 @@
 package com.github.talkbacktutorial.activities.modules.opennotifications;
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
 import com.github.talkbacktutorial.databinding.FragmentOpenNotificationBinding
 
+
 class OpenNotificationModuleFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = OpenNotificationModuleFragment()
+    }
+
     private lateinit var binding: FragmentOpenNotificationBinding
     private lateinit var ttsEngine: TextToSpeechEngine
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_open_notification, container,false)
+    lateinit var mainView: ConstraintLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        this.binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_open_notification, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        this.ttsEngine = TextToSpeechEngine((activity as OpenNotificationModuleActivity))
-            .onFinishedSpeaking(triggerOnce = true) {
-            }
+        this.ttsEngine = TextToSpeechEngine((activity as OpenNotificationModuleActivity)).onFinishedSpeaking(triggerOnce = true) {
+            this.observeUser()
+        }
         this.speakIntro()
     }
 
-    override fun onDestroyView() {
-        this.ttsEngine.shutDown()
-        super.onDestroyView()
+    private fun observeUser() {
+        var viewChangeCounter = 0
+        addOnWindowFocusChangeListener {
+            speakFeedback(viewChangeCounter)
+            viewChangeCounter++;
+        }
     }
+
+    private fun speakFeedback(counter: Int) {
+        if (counter == 0) {
+            ttsEngine.speak("Good job. You have opened the notification drawer. Now try to close by doing the same gesture but" +
+                    " this time swipe from bottom to top")
+        } else if (counter == 1) {
+            ttsEngine.speak("Well done. The notification drawer has been closed. Let's try to open it one more time")
+        } else if (counter == 2) {
+            ttsEngine.speak("You have done it again. Now close it to move on to new lesson")
+        } else if (counter == 3) {
+            //move the user to the next lesson/module
+        }
+    }
+
+    private fun Fragment.addOnWindowFocusChangeListener(callback: (hasFocus: Boolean) -> Unit) =
+        view?.viewTreeObserver?.addOnWindowFocusChangeListener(callback)
+
+    private fun Fragment.removeOnWindowFocusChangeListener(callback: (hasFocus: Boolean) -> Unit) =
+        view?.viewTreeObserver?.removeOnWindowFocusChangeListener(callback)
+
 
     /**
      * Speaks an intro for the fragment.
@@ -63,5 +99,10 @@ class OpenNotificationModuleFragment : Fragment() {
         this.ttsEngine.onFinishedSpeaking {
         }
         this.speakOutro()
+    }
+
+    override fun onDestroyView() {
+        this.ttsEngine.shutDown()
+        super.onDestroyView()
     }
 }
