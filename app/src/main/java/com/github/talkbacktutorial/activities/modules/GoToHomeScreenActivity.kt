82f0f41f7b23f61a7c2d40e1e5f5c9f0eddc790f
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-05-06 18:39:20 +1000

    basic implementation of gotohomescreenmodule

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/GoToHomeScreenActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/GoToHomeScreenActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
 
-class GoToHomeScreenModule : AppCompatActivity() {
+class GoToHomeScreenActivity : AppCompatActivity() {
     private lateinit var ttsEngine: TextToSpeechEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +19,11 @@ class GoToHomeScreenModule : AppCompatActivity() {
         val intro = """
             Welcome.
             In this module, you'll learn how to return to the home screen from inside an application,
-            to perform this task, swipe up then left. Try it now.
+            to perform this task, swipe up then left. Try it now. But return to the tutorial after you
+            reached the home screen.
         """.trimIndent()
+        // This is a very basic implementation that just asks the user to return
+        // to the application by themselves
         this.ttsEngine.speakOnInitialisation(intro)
     }
 }
--- app/src/main/java/com/github/talkbacktutorial/lessons/modules/GoToHomeScreenModule.kt
+++ app/src/main/java/com/github/talkbacktutorial/lessons/modules/GoToHomeScreenModule.kt
@@ -2,6 +2,7 @@ package com.github.talkbacktutorial.lessons.modules
 
 import android.content.Context
 import android.content.Intent
+import com.github.talkbacktutorial.activities.modules.GoToHomeScreenActivity
 import com.github.talkbacktutorial.activities.modules.TodoModuleXModuleActivity
 
 /**
@@ -12,7 +13,7 @@ class GoToHomeScreenModule : Module() { // E.g. PausingAndPlayingMediaModule
     override val title: String = "Go To Home Screen" // E.g. "Pause and Play Media"
 
     override fun startActivity(context: Context) {
-        context.startActivity(Intent(context, GoToHomeScreenModule::class.java))
+        context.startActivity(Intent(context, GoToHomeScreenActivity::class.java))
     }
 
 }
