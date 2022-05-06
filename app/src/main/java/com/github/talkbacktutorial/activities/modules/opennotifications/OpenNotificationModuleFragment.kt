1ef99a7c27944bdc6ceeede338d6e5dbed20e388
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2022-05-07 02:29:50 +1000

    Add meaningful documentation

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
@@ -20,8 +20,6 @@ class OpenNotificationModuleFragment : Fragment() {
     private lateinit var binding: FragmentOpenNotificationBinding
     private lateinit var ttsEngine: TextToSpeechEngine
 
-    lateinit var mainView: ConstraintLayout
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,12 +32,21 @@ class OpenNotificationModuleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        this.ttsEngine = TextToSpeechEngine((activity as OpenNotificationModuleActivity)).onFinishedSpeaking(triggerOnce = true) {
-            this.observeUser()
-        }
+        this.ttsEngine =
+            TextToSpeechEngine((activity as OpenNotificationModuleActivity)).onFinishedSpeaking(
+                triggerOnce = true
+            ) {
+                //Trigger this function once the intro is done speaking
+                this.observeUser()
+            }
         this.speakIntro()
     }
 
+    /**
+     * This function observe and count the view changes i.e., everytime the user open or close the notification and
+     * tell them what to do next
+     * @author Vinh Tuan Huynh
+     */
     private fun observeUser() {
         var viewChangeCounter = 0
         addOnWindowFocusChangeListener {
@@ -48,22 +55,44 @@ class OpenNotificationModuleFragment : Fragment() {
         }
     }
 
+    /**
+     * This function provides different feedback based on the number of times the user
+     * successfully open/close notification
+     * @author Vinh Tuan Huynh
+     */
     private fun speakFeedback(counter: Int) {
         if (counter == 0) {
-            ttsEngine.speak("Good job. You have opened the notification drawer. Now try to close by doing the same gesture but" +
-                    " this time swipe from bottom to top")
+            //Once the user open it. Teach them how to close it
+            ttsEngine.speak(
+                "Good job. You have opened the notification drawer. Now try to close by doing the same gesture but" +
+                        " this time start from the bottom to top"
+            )
         } else if (counter == 1) {
-            ttsEngine.speak("Well done. The notification drawer has been closed. Let's try to open it one more time")
+            //Once the user close it. Teach them how to do it by swiping right then down
+            ttsEngine.speak(
+                "Well done. The notification drawer has been closed. Let's try to open it one more time." +
+                        "But this time, open it by swiping right then down immediately."
+            )
         } else if (counter == 2) {
-            ttsEngine.speak("You have done it again. Now close it to move on to new lesson")
+            //Tell the user to close it again
+            ttsEngine.speak("Excellent. Now close it to move on to new lesson")
         } else if (counter == 3) {
             //move the user to the next lesson/module
         }
     }
 
+    /**
+     * This function adds a window focus change listener. Basically, this listener will call the callback func everytime
+     * we do something that alternate the view (window focus change) e.g., open the notification drawer or any kind of drawer.
+     * @author: Vinh Tuan Huynh
+     */
     private fun Fragment.addOnWindowFocusChangeListener(callback: (hasFocus: Boolean) -> Unit) =
         view?.viewTreeObserver?.addOnWindowFocusChangeListener(callback)
 
+    /**
+     * This function delete the listener added above
+     * @author: Vinh Tuan Huynh
+     */
     private fun Fragment.removeOnWindowFocusChangeListener(callback: (hasFocus: Boolean) -> Unit) =
         view?.viewTreeObserver?.removeOnWindowFocusChangeListener(callback)
 
@@ -77,8 +106,7 @@ class OpenNotificationModuleFragment : Fragment() {
             Welcome. In this module, you'll learn how to open your notifications. 
             Notifications are a way to let you know that something new has happened so you don't miss
             anything that might be worth your attention and appears whether you are using the application or not
-            To do this, simply swipe right then swipe down immediately. Alternatively, you can also do this by 
-            using 2 fingers and swipe from top to bottom.
+            There are two way to do this. First, let's open the notification drawer by putting 2 finger on top of the screen then swipe down.
         """.trimIndent()
         this.ttsEngine.speakOnInitialisation(intro)
     }
@@ -89,8 +117,8 @@ class OpenNotificationModuleFragment : Fragment() {
      */
     private fun speakOutro() {
         val outro = """
-            Well done!
-            You now know how to open and check your notifications. 
+            Well done! You now know how to open and check and close your notification drawer. 
+            Notification drawer can also be used to perform many kind of shortcut. For example, connect or disconnect wifi or bluetooth.
         """.trimIndent()
         this.ttsEngine.speak(outro)
     }
