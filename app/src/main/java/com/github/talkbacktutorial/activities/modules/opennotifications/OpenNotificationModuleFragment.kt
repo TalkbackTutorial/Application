ae0ee5ffcfe0d466d9eac6c4bee6a0c251cc2cb3
Vinh Tuan Huynh <thuynh2001.dw@gmail.com>
2022-05-07 17:22:56 +1000

    Add delay timer so Talkback and tts does not overlap

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/opennotifications/OpenNotificationModuleFragment.kt
@@ -1,15 +1,18 @@
-package com.github.talkbacktutorial.activities.modules.opennotifications;
+package com.github.talkbacktutorial.activities.modules.opennotifications
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
-import android.view.accessibility.AccessibilityEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
 import com.github.talkbacktutorial.activities.MainActivity
 import com.github.talkbacktutorial.databinding.FragmentOpenNotificationBinding
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 
 class OpenNotificationModuleFragment : Fragment() {
@@ -22,7 +25,7 @@ class OpenNotificationModuleFragment : Fragment() {
     private lateinit var binding: FragmentOpenNotificationBinding
     private lateinit var ttsEngine: TextToSpeechEngine
 
-    var viewChangeCounter = 0
+    private var viewChangeCounter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,7 +38,6 @@ class OpenNotificationModuleFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.accessibilityDelegate = InterceptorDelegate()
         super.onViewCreated(view, savedInstanceState)
         this.ttsEngine =
             TextToSpeechEngine((activity as OpenNotificationModuleActivity)).onFinishedSpeaking(
@@ -54,8 +56,10 @@ class OpenNotificationModuleFragment : Fragment() {
      */
     private fun observeUser() {
         addOnWindowFocusChangeListener {
-            speakFeedback(viewChangeCounter)
-            viewChangeCounter++;
+            Timer().schedule(1000) {
+                speakFeedback(viewChangeCounter)
+                viewChangeCounter++
+            }
         }
     }
 
@@ -66,22 +70,30 @@ class OpenNotificationModuleFragment : Fragment() {
      */
     private fun speakFeedback(counter: Int) {
         if (counter == 0) {
-            //Once the user open it. Teach them how to close it
-            ttsEngine.speak(
-                "Good job. You have opened the notification drawer. Now try to close it by doing the same gesture but" +
-                        " this time start from the bottom to top"
-            )
+            // once the user open noti panel. Teach them how to close it. Since Talkback will be talking a lot here and there is no way to stop it,
+            // extend the timer so tts engine can talk after Talkback is done talking.
+            // ***Note that the delay time does not have to be exact it can be anything as long as it is after Talkback start ranting :)
+            Timer().schedule(3000) {
+                ttsEngine.speak(
+                    "Good job. You have opened the notification panel. Now try to close it by doing the same gesture but" +
+                            " this time start from the bottom to top"
+                )
+            }
         } else if (counter == 1) {
-            //Once the user close it. Teach them how to do it by swiping right then down
+            // once the user close the panel. Teach them how to do it by swiping right then down
             ttsEngine.speak(
-                "Well done. The notification drawer has been closed. Let's try to open it one more time" +
+                "Well done. The notification panel has been closed. Let's try to open it one more time" +
                         "but this time, open it by swiping right, then down immediately."
             )
+
         } else if (counter == 2) {
-            //Tell the user to close it again
-            ttsEngine.speak("Excellent. Now close it to move on to new lesson")
+            // tell the user to close the panel after it is opened again
+            Timer().schedule(3000) {
+                ttsEngine.speak("Excellent. Now close it again by swiping from the bottom with 2 finger")
+            }
+
         } else if (counter == 3) {
-            //move the user to the next lesson/module
+            // move the user to the next lesson/module
             viewChangeCounter = 0
             finishLesson()
         }
@@ -109,9 +121,8 @@ class OpenNotificationModuleFragment : Fragment() {
      */
     private fun speakIntro() {
         val intro = """
-            Welcome. In this module, you'll learn how to open your notifications. 
-            Notifications are a way to let you know that something new has happened so you don't miss
-            anything that might be worth your attention and appears whether you are using the application or not
+            Welcome. In this module, you'll learn how to open your notification panel. 
+            Notifications are a way to let you know that something new has happened so you don't miss anything that might be worth your attention and appears whether you are using the application or not
             There are two way to do this. First, let's open the notification drawer by putting 2 finger on top of the screen then swipe down.
         """.trimIndent()
         this.ttsEngine.speakOnInitialisation(intro)
@@ -123,8 +134,8 @@ class OpenNotificationModuleFragment : Fragment() {
      */
     private fun speakOutro() {
         val outro = """
-            Well done! You now know how to open and check and close your notification drawer. 
-            Notification drawer can also be used to perform many kind of shortcut. For example, connect or disconnect wifi or bluetooth.
+            Well done! You now know how to open and check and close your notification panel. 
+            Notification panel can also be used to perform many kind of shortcut. For example, connect or disconnect wifi or bluetooth.
             You have completed the lesson. We will send you back.
         """.trimIndent()
         this.ttsEngine.speak(outro)
@@ -149,19 +160,4 @@ class OpenNotificationModuleFragment : Fragment() {
         this.ttsEngine.shutDown()
         super.onDestroyView()
     }
-
-    private class InterceptorDelegate : View.AccessibilityDelegate() {
-        private lateinit var ttsEngine: TextToSpeechEngine
-
-        override fun onRequestSendAccessibilityEvent(
-            host: ViewGroup?,
-            child: View?,
-            event: AccessibilityEvent?
-        ): Boolean {
-            if (event?.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED) {
-            }
-            return super.onRequestSendAccessibilityEvent(host, child, event)
-        }
-    }
-
 }
