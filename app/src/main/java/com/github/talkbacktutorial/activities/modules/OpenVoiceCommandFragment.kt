ac5c9d95406a07c447624e2f00628b3cdd88c97a
Andre Pham <andrepham33@gmail.com>
2022-05-07 10:50:38 +1000

    Revert "Merge branch 'develop' into team2/sprint1"
    This reverts commit 0929f305828c1d21d0737ca29d19328a912c5cf2, reversing
    changes made to 75c41f8b0e404d911741f7e78215fbabc62a7577.

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/OpenVoiceCommandFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.github.talkbacktutorial.activities.modules
-
-import androidx.fragment.app.Fragment
-
-class OpenVoiceCommandFragment : Fragment() {
-}
--- app/src/main/java/com/github/talkbacktutorial/lessons/LessonContainer.kt
+++ app/src/main/java/com/github/talkbacktutorial/lessons/LessonContainer.kt
@@ -11,8 +11,7 @@ object LessonContainer {
     private val lessons = ArrayList<Lesson>(listOf(
         Lesson0(),
         Lesson1(),
-        Lesson2(),
-        Lesson5()
+        Lesson2()
     ))
 
     /**
