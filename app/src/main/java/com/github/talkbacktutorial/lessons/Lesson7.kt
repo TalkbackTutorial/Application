1cc87d2156658e583a1d4d7c3dfbfe725680c8a3
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-05-14 12:05:19 +1000

    added lesson 7 empty class

---
+++ app/src/main/java/com/github/talkbacktutorial/lessons/Lesson7.kt
@@ -0,0 +1,15 @@
+package com.github.talkbacktutorial.lessons
+
+import com.github.talkbacktutorial.lessons.modules.*
+import java.util.ArrayList
+
+class Lesson7 : Lesson() {
+    override val title: String = ""
+    override val sequenceNumeral: Int = 7
+    override val modules: ArrayList<Module> = ArrayList(
+            listOf(
+
+            )
+    )
+
+}
