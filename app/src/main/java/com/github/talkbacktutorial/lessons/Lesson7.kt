24171d549851b47a2f113ccc7cd7f248e8b97fc5
Mingxuan Fu <mfuu0003@student.monash.edu>
2022-05-14 12:06:30 +1000

    added lesson 7 name

--- app/src/main/java/com/github/talkbacktutorial/lessons/Lesson7.kt
+++ app/src/main/java/com/github/talkbacktutorial/lessons/Lesson7.kt
@@ -4,7 +4,7 @@ import com.github.talkbacktutorial.lessons.modules.*
 import java.util.ArrayList
 
 class Lesson7 : Lesson() {
-    override val title: String = ""
+    override val title: String = "Typing using braille keyboard"
     override val sequenceNumeral: Int = 7
     override val modules: ArrayList<Module> = ArrayList(
             listOf(
