53216544672d1406b6db00ddd7dedbd3ec652319
Alex Zhou <azho0003@student.monash.edu>
2022-05-18 20:06:17 +1000

    Setup xml files & added intro for Lesson 6

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/virtualkeyboard/VirtualKeyboardActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/virtualkeyboard/VirtualKeyboardActivity.kt
@@ -5,18 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import com.github.talkbacktutorial.R
+import com.github.talkbacktutorial.databinding.ActivityVirtualKeyboardModuleBinding
 
 class VirtualKeyboardActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityStartStopMediaModuleBinding
+    lateinit var binding: ActivityVirtualKeyboardModuleBinding
         private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_start_stop_media_module)
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_virtual_keyboard_module)
         supportFragmentManager.commit {
-            replace(R.id.frame1, StartStopMediaPart1Fragment.newInstance())
+            replace(R.id.frame1, VirtualKeyboardPart1Fragment.newInstance())
         }
     }
 }
--- app/src/main/java/com/github/talkbacktutorial/activities/modules/virtualkeyboard/VirtualKeyboardPart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/virtualkeyboard/VirtualKeyboardPart1Fragment.kt
@@ -1,7 +1,50 @@
 package com.github.talkbacktutorial.activities.modules.virtualkeyboard
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.github.talkbacktutorial.R
+import com.github.talkbacktutorial.TextToSpeechEngine
+import com.github.talkbacktutorial.databinding.FragmentVirtualKeyboardModulePart1Binding
 
 class VirtualKeyboardPart1Fragment : Fragment() {
 
+    companion object {
+        @JvmStatic
+        fun newInstance() = VirtualKeyboardPart1Fragment()
+    }
+
+    private lateinit var binding: FragmentVirtualKeyboardModulePart1Binding
+    private lateinit var ttsEngine: TextToSpeechEngine
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_virtual_keyboard_module_part1, container, false)
+        this.binding.virtualKeyboardConstraintLayout.visibility = View.INVISIBLE
+        return this.binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.ttsEngine = TextToSpeechEngine((activity as VirtualKeyboardActivity)).onFinishedSpeaking(triggerOnce = true) {
+            this.binding.virtualKeyboardConstraintLayout.visibility = View.VISIBLE
+        }
+        this.speakIntro()
+    }
+
+    private fun speakIntro() {
+        val intro = """
+            In this tutorial, you will be learning how to open the on-screen virtual keyboard and 
+            type using it. To start, explore by touch to find the text box then double tap to open
+            the virtual keyboard. 
+        """.trimIndent()
+
+        this.ttsEngine.speakOnInitialisation(intro)
+    }
 }
