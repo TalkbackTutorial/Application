ac9d6090c123381921481952ffe725e43245b49b
Vinh Tuan Huynh <you@example.com>
2022-05-20 17:42:29 +1000

    Add intro and outro

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/opentalkbackmenu/OpenTalkBackMenuPart1Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/opentalkbackmenu/OpenTalkBackMenuPart1Fragment.kt
@@ -1,5 +1,6 @@
 package com.github.talkbacktutorial.activities.modules.opentalkbackmenu
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
+import com.github.talkbacktutorial.activities.MainActivity
+import com.github.talkbacktutorial.activities.modules.opennotifications.OpenNotificationActivity
 import com.github.talkbacktutorial.databinding.FragmentOpenTalkbackMenuModulePart1Binding
+import java.util.*
+import kotlin.concurrent.schedule
 
 class OpenTalkBackMenuPart1Fragment : Fragment() {
 
@@ -19,6 +24,7 @@ class OpenTalkBackMenuPart1Fragment : Fragment() {
 
     private lateinit var binding: FragmentOpenTalkbackMenuModulePart1Binding
     private lateinit var ttsEngine: TextToSpeechEngine
+    private var viewChangeCounter = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +32,60 @@ class OpenTalkBackMenuPart1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         this.binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_open_notification_module_part1, container, false)
+            DataBindingUtil.inflate(inflater, R.layout.fragment_open_talkback_menu_module_part_1, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.ttsEngine =
+            TextToSpeechEngine((activity as OpenNotificationActivity)).onFinishedSpeaking(
+                triggerOnce = true
+            ) {
+                // Trigger this function once the intro is done speaking
+            }
+        this.speakIntro()
+    }
+
+    /**
+     * Speaks an intro for the fragment.
+     * @author Vinh Tuan Huynh
+     */
+    private fun speakIntro() {
+        val intro = """
+            Welcome. In this module, you'll learn how to open Talkback Menu.
+            Talkback menu let you use commands to read, edit text, control speech output, change Talkback setting and so.
+        """.trimIndent()
+        this.ttsEngine.speakOnInitialisation(intro)
+    }
+
+    /**
+     * Speaks an outro for the fragment.
+     * @author Vinh Tuan Huynh
+     */
+    private fun speakOutro() {
+        val outro = """
+            Well done! You now know how to open and close Talkback Menu. You will be moved to a new lesson.
+        """.trimIndent()
+        this.ttsEngine.speak(outro)
+    }
+
+    /**
+     * To do when finish the lesson
+     * @author Vinh Tuan Huynh
+     */
+    private fun finishLesson() {
+        this.ttsEngine.onFinishedSpeaking(triggerOnce = true) {
+            val intent =
+                Intent((activity as OpenNotificationActivity), MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+        this.speakOutro()
+    }
+
+    override fun onDestroyView() {
+        this.ttsEngine.shutDown()
+        super.onDestroyView()
     }
 }
