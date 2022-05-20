0e9f3056ed444be164d952bd2b8258b9aabb2e78
nlaw0004 <nlaw0004@student.monash.edu>
2022-05-20 14:15:05 +1000

    change default m3 background, add description for lesson6

--- app/src/main/java/com/github/talkbacktutorial/lessons/Lesson6.kt
+++ app/src/main/java/com/github/talkbacktutorial/lessons/Lesson6.kt
@@ -6,6 +6,7 @@ class Lesson6 : Lesson() {
 
     override val title: String = "Typing Lesson"
     override val sequenceNumeral: Int = 6
+    override val description = "These cover the gestures and actions to use a virtual keyboard on the phone"
     override val modules: ArrayList<Module> = ArrayList(
         listOf(
             VirtualKeyboardModule()
