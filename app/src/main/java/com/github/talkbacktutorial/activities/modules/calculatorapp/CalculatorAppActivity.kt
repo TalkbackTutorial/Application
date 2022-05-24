de2b0bbbf269bfe97f5c1e594920da9ef90193a0
Chokss54 <joelyang54@gmail.com>
2022-05-24 13:04:18 +0800

    - modify activity to handle incoming intents to open different fragments

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/calculatorapp/CalculatorAppActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/calculatorapp/CalculatorAppActivity.kt
@@ -1,17 +1,47 @@
 package com.github.talkbacktutorial.activities.modules.calculatorapp
 
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.activities.modules.explorebytouch.ExploreMenuByTouchPart1Fragment
+import com.github.talkbacktutorial.activities.modules.voicerecorderapp.VoiceRecorderAppActivity
+import com.github.talkbacktutorial.activities.modules.voicerecorderapp.VoiceRecorderFinishedFragment
+import com.github.talkbacktutorial.activities.modules.voicerecorderapp.VoiceRecorderPlayRecordingFragment
 import com.github.talkbacktutorial.databinding.ActivityCalculatorAppModuleBinding
 import com.github.talkbacktutorial.databinding.ActivityExploreMenuByTouchModuleBinding
 
 class CalculatorAppActivity : AppCompatActivity() {
+    private val TUTORIAL_PREFIX = "TBT"
+    private val EXTERNAL_APP_TAG = "SC"
+    private val APP_ACTION_KEY = "${TUTORIAL_PREFIX}_${EXTERNAL_APP_TAG}_ACTION"
+    private val FINISH_CALCULATOR_TAG = "${EXTERNAL_APP_TAG}_TASK_FINISH_CALCULATOR"
+
     lateinit var binding: ActivityCalculatorAppModuleBinding
         private set
+
+    companion object {
+        private val appIntent = Intent()
+
+        /**
+         * Gets the intent to open the external app needed by this activity and its fragments.
+         *
+         * @return an intent that can be used with startActivity
+         */
+        fun getAppIntent(): Intent {
+            appIntent.component = ComponentName(
+                "com.simplemobiletools.calculatorapp.debug",
+                "com.simplemobiletools.calculator.activities.SplashActivity.Orange"
+            )
+
+            return appIntent
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +49,32 @@ class CalculatorAppActivity : AppCompatActivity() {
         this.binding = DataBindingUtil.setContentView(this, R.layout.activity_calculator_app_module)
         supportFragmentManager.commit {
             replace(R.id.frame, CalculatorAppPart1Fragment.newInstance())
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        /*
+            New intent usually means this activity was called from elsewhere after first start.
+            This is probably the external app.
+         */
+        intent?.let {
+            // get extras
+            val actionCompleted = intent.getStringExtra(APP_ACTION_KEY)
+
+            actionCompleted?.let {
+                // move on with module based on tag IF intro is the current fragment
+                 if (actionCompleted == FINISH_CALCULATOR_TAG) {
+
+                    supportFragmentManager.commit {
+                        replace(R.id.frame, CalculatorAppPart2Fragment())
+                    }
+                } else {
+                    // wrong move
+
+                }
+            }
         }
     }
 }
