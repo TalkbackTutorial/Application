0a1190e595767d2859059874c2346301aaae411d
Chokss54 <joelyang54@gmail.com>
2022-05-24 13:05:14 +0800

    - create finish module fragment for calculator app - minor updates

--- app/src/main/java/com/github/talkbacktutorial/activities/modules/calculatorapp/CalculatorAppPart2Fragment.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/modules/calculatorapp/CalculatorAppPart2Fragment.kt
@@ -10,16 +10,15 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
-import com.github.talkbacktutorial.activities.modules.jumptext.JumpTextActivity
-import com.github.talkbacktutorial.databinding.FragmentCalculatorAppModulePart1Binding
+import com.github.talkbacktutorial.databinding.FragmentCalculatorAppModulePart2Binding
 
-class CalculatorAppPart1Fragment : Fragment() {
-    private lateinit var binding: FragmentCalculatorAppModulePart1Binding
+class CalculatorAppPart2Fragment : Fragment() {
+    private lateinit var binding: FragmentCalculatorAppModulePart2Binding
     private lateinit var ttsEngine: TextToSpeechEngine
 
     companion object {
         @JvmStatic
-        fun newInstance() = CalculatorAppPart1Fragment()
+        fun newInstance() = CalculatorAppPart2Fragment()
     }
 
     override fun onCreateView(
@@ -28,7 +27,7 @@ class CalculatorAppPart1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         this.binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_calculator_app_module_part1, container, false)
+            DataBindingUtil.inflate(inflater, R.layout.fragment_calculator_app_module_part2, container, false)
         return binding.root
     }
 
@@ -38,7 +37,7 @@ class CalculatorAppPart1Fragment : Fragment() {
             .onFinishedSpeaking(triggerOnce = true) {
                 binding.continueCard.visibility = View.VISIBLE
             }
-        this.setupContinueCard()
+        this.setupFinishCard()
         this.speakIntro()
     }
 
@@ -47,25 +46,23 @@ class CalculatorAppPart1Fragment : Fragment() {
      * @author Joel Yang
      */
     private fun speakIntro() {
-        val intro = "In this module, you will apply what you have learnt from the previous lessons to use the simple calculator app. You will be given a simple calculation task to work on. Unless you complete this task successfully, we will let you retry as many times".trimIndent()
+        val intro = "You have successfully completed this module. We have sent you back to the talkback module. please proceed to finish module.".trimIndent()
         this.ttsEngine.speakOnInitialisation(intro)
     }
 
     /**
-     * A card which starts a new fragment when interacted by user
+     * A card which sends user back to the module when interacted by user
      * @author Joel Yang
      */
-    private fun setupContinueCard() {
+    private fun setupFinishCard() {
         // The card starts off invisible
         binding.continueCard.visibility = View.GONE
         // The button transitions to the next fragment when clicked
         binding.continueCard.setOnClickListener {
-            val intent = Intent()
-
-            // target the main activity of the forked calculator recorder app
-            intent.component = ComponentName("com.simplemobiletools.calculator.debug", "com.simplemobiletools.calculator.activities.SplashActivity.Orange")
-
-            startActivity(intent)
+            this.ttsEngine.onFinishedSpeaking(triggerOnce = true) {
+                activity?.onBackPressed()
+            }
+            this.ttsEngine.speak(getString(R.string.finish_calculator_module), override = true)
         }
     }
 
