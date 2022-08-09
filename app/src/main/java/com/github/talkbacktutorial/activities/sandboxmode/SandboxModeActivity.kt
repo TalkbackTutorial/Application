f05369279a7cbcaad14675f3da153317980a0a71
Dongzheng Wu <dwuu0018@student.monash.edu>
2022-08-09 15:37:16 +1000

    Completed enter and exit sandbox mode using shortcut

--- app/src/main/java/com/github/talkbacktutorial/activities/sandboxmode/SandboxModeActivity.kt
+++ app/src/main/java/com/github/talkbacktutorial/activities/sandboxmode/SandboxModeActivity.kt
@@ -1,12 +1,20 @@
 package com.github.talkbacktutorial.activities.sandboxmode
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MotionEvent
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.github.talkbacktutorial.R
 import com.github.talkbacktutorial.TextToSpeechEngine
+import com.github.talkbacktutorial.accessibilitymanager.AccessibilityChangeListener
+import com.github.talkbacktutorial.accessibilitymanager.AccessibilityChangeManager
+import com.github.talkbacktutorial.accessibilitymanager.AccessibilityChangePage
+import com.github.talkbacktutorial.activities.MainActivity
 import com.github.talkbacktutorial.databinding.ActivitySandboxModeBinding
 import com.github.talkbacktutorial.gestures.delegates.GestureDelegate
 import com.github.talkbacktutorial.gestures.GestureIdentifier
@@ -25,9 +33,38 @@ class SandboxModeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         this.binding = DataBindingUtil.setContentView(this, R.layout.activity_sandbox_mode)
+        this.toggleInterface(false)
+
+        AccessibilityChangeManager.setListener(
+            AccessibilityChangeListener(
+                talkbackOnCallback = {
+                    this.toggleInterface(false)
+                    this.ttsEngine.onFinishedSpeaking(triggerOnce = true) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(intent)
+                    }
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        this.ttsEngine.speak("Sandbox ended. Sending you to the main menu.", override = true)
+                    }, 2000)    // Avoid conflicts with reading "talkback off"
+                },
+                talkbackOffCallback = {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        this.ttsEngine.onFinishedSpeaking(triggerOnce = true) {
+                            this.toggleInterface(true)
+                        }
+                        this.ttsEngine.speak("Sandbox starting. Remember, to exit, hold down both volume keys on the side of your device simultaneously. To use the sandbox, perform the correct gesture and the action of the gesture will be spoken. The sandbox will start now.")
+                    }, 3000)    // Avoid conflicts with reading "talkback on, talkback tutorial"
+                },
+                associatedPage = AccessibilityChangePage.SANDBOX
+            )
+        )
 
         // initialise tts
         this.ttsEngine = TextToSpeechEngine(this)
+        this.ttsEngine.speakOnInitialisation(
+            "Welcome. To start or end the sandbox, you'll need to disable Talkback by holding down both volume keys on the side of your device simultaneously."
+        )
 
         // Setup delegates to feed data to gestureIdentifier
         this.gestureDelegate = GestureDelegate(this.gestureIdentifier.gestureData)
@@ -50,5 +87,23 @@ class SandboxModeActivity : AppCompatActivity() {
             }
             return@setOnTouchListener true
         }
+    }
+
+    /**
+     * Toggle whether or not the interface is shown.
+     * @author Andre Pham
+     */
+    private fun toggleInterface(show: Boolean) {
+        this.binding.touchOverlay.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    override fun onResume() {
+        AccessibilityChangeManager.setPage(AccessibilityChangePage.SANDBOX)
+        super.onResume()
+    }
+
+    override fun onStop() {
+        AccessibilityChangeManager.resetPage(AccessibilityChangePage.SANDBOX)
+        super.onStop()
     }
 }
